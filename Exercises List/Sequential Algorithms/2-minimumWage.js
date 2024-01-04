379e213a18a42fc3f04cda0b924e6b8324195531
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2024-01-04 18:03:45 -0300

    Update 2-minimumWage.js

--- Exercises List/Sequential Algorithms/2-minimumWage.js
+++ Exercises List/Sequential Algorithms/2-minimumWage.js
@@ -2,8 +2,8 @@
 
 /*********************************************************************************************************
  *
- * Faça um algoritmo que receba o valor do salário mínimo e o valor do salário de um funcionário
- * Calcule e mostre a quantidade de salários mínimos que ganha esse funcionário
+ * Faça um algoritmo que receba o valor do salário mínimo e o valor do salário de um funcionário.
+ * Calcule e mostre a quantidade de salários mínimos que ganha esse funcionário.
  *
  *********************************************************************************************************/
 
